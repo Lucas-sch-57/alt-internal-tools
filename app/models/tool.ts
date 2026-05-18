7b6bcc07803ee703443a5711cffffe81224ee5da
SCHILTZ Lucas <lucasschiltz.etude57@gmail.com>
2026-05-18 11:23:02 +0200

    feat: Add relationship in tool model + Wrtie the tool transformer

--- app/models/tool.ts
+++ app/models/tool.ts
@@ -1,4 +1,9 @@
 import { ToolSchema } from '#database/schema'
+import { belongsTo } from '@adonisjs/lucid/orm'
+import Category from './category.ts'
+import type { BelongsTo } from '@adonisjs/lucid/types/relations'
 
 export default class Tool extends ToolSchema {
+  @belongsTo(() => Category)
+  declare category: BelongsTo<typeof Category>
 }
