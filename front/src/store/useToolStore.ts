66170049aa86c4a7518160a8601a88bbf44672cd
SCHILTZ Lucas <lucasschiltz.etude57@gmail.com>
2026-05-21 10:57:08 +0200

    feat: Update useToolStore to implements filters system on tools page

--- front/src/store/useToolStore.ts
+++ front/src/store/useToolStore.ts
@@ -12,11 +12,26 @@ interface ToolStore {
   filters: ToolFilters;
   setFilter: (key: keyof ToolFilters, value: any) => void;
   resetFilters: () => void;
+  selectedTools: number[];
+  toggleSelect: (id: number) => void;
+  selectAll: (ids: number[]) => void;
+  clearSelection: () => void;
 }
 
 export const useToolStore = create<ToolStore>(set => ({
   filters: {},
   setFilter: (key, value) =>
-    set(state => ({ filters: { ...state.filters, [key]: value } })),
+    set(state => ({
+      filters: { ...state.filters, [key]: value === '' ? undefined : value },
+    })),
   resetFilters: () => set({ filters: {} }),
+  selectedTools: [],
+  toggleSelect: id =>
+    set(state => ({
+      selectedTools: state.selectedTools.includes(id)
+        ? state.selectedTools.filter(i => i !== id)
+        : [...state.selectedTools, id],
+    })),
+  selectAll: ids => set({ selectedTools: ids }),
+  clearSelection: () => set({ selectedTools: [] }),
 }));
